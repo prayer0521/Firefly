4c0add824a237b028ae4d517b9513b96a0b20e05
prayer0521 <3025249664@qq.com>
2026-06-01 16:58:39 +0800

    图片更新

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -47,6 +47,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			"assets/images/DesktopWallpaper/d4.avif",
 			"assets/images/DesktopWallpaper/d5.avif",
 			"assets/images/DesktopWallpaper/d6.avif",
+			"assets/images/DesktopWallpaper/d7.avif",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
@@ -57,6 +58,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			"assets/images/MobileWallpaper/m4.avif",
 			"assets/images/MobileWallpaper/m5.avif",
 			"assets/images/MobileWallpaper/m6.avif",
+			"assets/images/MobileWallpaper/m7.avif",
 		],
 	},
 	// 横幅壁纸和全屏壁纸共享配置
@@ -70,17 +72,17 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否允许用户通过控制面板切换横幅标题显示
 			switchable: true,
 			// 主页横幅主标题
-			title: "Lovely firefly!",
+			title: "Hellooooo prayer!",
 			// 主页横幅主标题字体大小
 			titleSize: "3.8rem",
 			// 主页横幅副标题
 			subtitle: [
-				"In Reddened Chrysalis, I Once Rest",
-				"From Shattered Sky, I Free Fall",
-				"Amidst Silenced Stars, I Deep Sleep",
-				"Upon Lighted Fyrefly, I Soon Gaze",
-				"From Undreamt Night, I Thence Shine",
-				"In Finalized Morrow, I Full Bloom",
+				"In Silent Years, I Settle",
+				"From Fading Days, I Let Go",
+				"Amidst Quiet Waves, I Calm",
+				"Upon Gentle Truth, I Perceive",
+				"From Burdened Past, I Heal",
+				"In Peaceful Self, I Abide",
 			],
 			// 主页横幅副标题字体大小
 			subtitleSize: "1.5rem",

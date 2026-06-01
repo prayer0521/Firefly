d735d16fa6ee7cbe4e287507cf3a23fa6a7c40b4
prayer0521 <3025249664@qq.com>
2026-06-01 17:30:36 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -129,7 +129,7 @@ export const siteConfig: SiteConfig = {
 	// bangumi的数据为编译时获取的，所以不是实时数据，请配置bangumi.userId
 	pages: {
 		// 友链页面开关
-		friends: true,
+		friends: false,
 		// 赞助页面开关
 		sponsor: false,
 		// 留言板页面开关，需要配置评论系统

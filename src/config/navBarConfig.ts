d71690a27c2a4801359b6e0af22d7ace3b3a114c
prayer0521 <3025249664@qq.com>
2026-06-21 23:10:19 +0800

    删掉了相册

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -29,18 +29,18 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 	}
 
 	// 我的及其子菜单
-	links.push({
-		name: "我的",
-		url: "/my/",
-		icon: "material-symbols:person",
-		children: [
-			// 根据配置决定是否添加相册，在siteConfig关闭pages.gallery时导航栏不显示相册
-			...(siteConfig.pages.gallery ? [LinkPreset.Gallery] : []),
+	// links.push({
+	// 	name: "我的",
+	// 	url: "/my/",
+	// 	icon: "material-symbols:person",
+	// 	children: [
+	// 		// 根据配置决定是否添加相册，在siteConfig关闭pages.gallery时导航栏不显示相册
+	// 		...(siteConfig.pages.gallery ? [LinkPreset.Gallery] : []),
 
-			// 根据配置决定是否添加番组计划，在siteConfig关闭pages.bangumi时导航栏不显示番组计划
-			...(siteConfig.pages.bangumi ? [LinkPreset.Bangumi] : []),
-		],
-	});
+	// 		// 根据配置决定是否添加番组计划，在siteConfig关闭pages.bangumi时导航栏不显示番组计划
+	// 		...(siteConfig.pages.bangumi ? [LinkPreset.Bangumi] : []),
+	// 	],
+	// });
 
 	// 关于及其子菜单
 	links.push({
